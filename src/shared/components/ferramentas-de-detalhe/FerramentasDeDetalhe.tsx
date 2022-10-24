118ba626cadc1ffb5d25ba1812c22e3c08b06c6d
Leonardo <leonardorodrigues@grendene.com.br>
2022-10-24 10:49:18 -0300

    Responsividade do item da barra de ferramentas

--- src/shared/components/ferramentas-de-detalhe/FerramentasDeDetalhe.tsx
+++ src/shared/components/ferramentas-de-detalhe/FerramentasDeDetalhe.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Divider, Icon, Paper, useTheme } from '@mui/material';
+import { Box, Button, Divider, Icon, Paper, Skeleton, Theme, Typography, useMediaQuery, useTheme } from '@mui/material';
 
 interface IFerramentasDeDetalheProps{
   textoBotaoNovo?: string,
@@ -7,6 +7,12 @@ interface IFerramentasDeDetalheProps{
   mostrarBotaoApagar?: boolean,
   mostrarBotaoSalvar?: boolean,
   mostrarBotaoSalvarEFechar?: boolean,
+
+  mostrarBotaoNovoCarregando?: boolean,
+  mostrarBotaoVoltarCarregando?: boolean,
+  mostrarBotaoApagarCarregando?: boolean,
+  mostrarBotaoSalvarCarregando?: boolean,
+  mostrarBotaoSalvarEFecharCarregando?: boolean,
 
   aoClicarEmNovo?: () => void,
   aoClicarEmVoltar?: () => void,
@@ -24,6 +30,12 @@ export function FerramentasDeDetalhe (
     mostrarBotaoSalvar = true,
     mostrarBotaoSalvarEFechar = false,
 
+    mostrarBotaoNovoCarregando = false,
+    mostrarBotaoVoltarCarregando = false,
+    mostrarBotaoApagarCarregando = false,
+    mostrarBotaoSalvarCarregando = false,
+    mostrarBotaoSalvarEFecharCarregando = false,
+
     aoClicarEmNovo,
     aoClicarEmVoltar,
     aoClicarEmApagar,
@@ -32,6 +44,9 @@ export function FerramentasDeDetalhe (
   } : IFerramentasDeDetalheProps
 ){
   const theme = useTheme();
+
+  const smDown = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'));
+  const mdDown = useMediaQuery((theme: Theme) => theme.breakpoints.down('md'));
 
   return(
     <Box
@@ -45,7 +60,7 @@ export function FerramentasDeDetalhe (
       component={Paper}
     >
 
-      {mostrarBotaoSalvar && (
+      {mostrarBotaoSalvar && !mostrarBotaoSalvarCarregando && (
         <Button
           color='primary'
           disableElevation
@@ -53,10 +68,19 @@ export function FerramentasDeDetalhe (
           onClick={aoClicarEmSalvar}
           startIcon={<Icon>save</Icon>}
         >
-        Salvar
+          <Typography 
+            variant='button' 
+            whiteSpace='nowrap'
+            textOverflow='ellipsis'
+            overflow='hidden'
+          >
+            Salvar
+          </Typography>
         </Button>
       )}
-      {mostrarBotaoSalvarEFechar && (
+      {mostrarBotaoSalvarCarregando && !smDown && (<Skeleton width={110} height={60}/>)}
+
+      {mostrarBotaoSalvarEFechar && !mostrarBotaoSalvarEFecharCarregando && !smDown && !mdDown && (
         <Button
           color='primary'
           disableElevation
@@ -64,10 +88,21 @@ export function FerramentasDeDetalhe (
           onClick={aoClicarEmSalvarEFechar}
           startIcon={<Icon>save</Icon>}
         >
-        Salvar e voltar
+          <Typography
+            variant='button'
+            whiteSpace='nowrap'
+            textOverflow='ellipsis'
+            overflow='hidden'
+          >
+            Salvar e voltar
+          </Typography>
         </Button>
       )}
-      {mostrarBotaoApagar && (
+      {mostrarBotaoSalvarEFecharCarregando && !smDown && !mdDown && (
+        <Skeleton width={180} height={60} />
+      )}
+
+      {mostrarBotaoApagar && !mostrarBotaoApagarCarregando && (
         <Button
           color='primary'
           disableElevation
@@ -75,10 +110,19 @@ export function FerramentasDeDetalhe (
           onClick={aoClicarEmApagar}
           startIcon={<Icon>delete</Icon>}
         >
-        Apagar
+          <Typography
+            variant='button'
+            whiteSpace='nowrap'
+            textOverflow='ellipsis'
+            overflow='hidden'
+          >
+            Apagar
+          </Typography>
         </Button>
       )}
-      {mostrarBotaoNovo && (
+      {mostrarBotaoApagarCarregando && !smDown && (<Skeleton width={110} height={60} />)}
+
+      {mostrarBotaoNovo && !mostrarBotaoNovoCarregando && !smDown && (
         <Button
           color='primary'
           disableElevation
@@ -86,12 +130,25 @@ export function FerramentasDeDetalhe (
           onClick={aoClicarEmNovo}
           startIcon={<Icon>add</Icon>}
         >
-          {textoBotaoNovo}
+          <Typography
+            variant='button'
+            whiteSpace='nowrap'
+            textOverflow='ellipsis'
+            overflow='hidden'
+          >
+            {textoBotaoNovo}
+          </Typography>
         </Button>
       )}      
-      <Divider variant='middle' orientation='vertical'/>
+      {mostrarBotaoNovoCarregando && !smDown && (<Skeleton width={110} height={60} />)}
 
       {mostrarBotaoVoltar && (
+        (mostrarBotaoNovo || mostrarBotaoApagar || mostrarBotaoSalvar || mostrarBotaoSalvarEFechar)
+      ) && (
+        <Divider variant='middle' orientation='vertical' />
+      )}
+
+      {mostrarBotaoVoltar && !mostrarBotaoVoltarCarregando &&(
         <Button
           color='primary'
           disableElevation
@@ -99,9 +156,17 @@ export function FerramentasDeDetalhe (
           onClick={aoClicarEmVoltar}
           startIcon={<Icon>arrow_back</Icon>}
         >
-        Voltar
+          <Typography
+            variant='button'
+            whiteSpace='nowrap'
+            textOverflow='ellipsis'
+            overflow='hidden'
+          >
+            Voltar
+          </Typography>
         </Button>
       )}
+      {mostrarBotaoVoltarCarregando && !smDown && (<Skeleton width={110} height={60} />)}
 
     </Box>
   );
