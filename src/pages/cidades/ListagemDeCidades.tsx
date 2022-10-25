18417ce4006461561d47b74b5afcbabdb0af8e09
Leonardo <leonardorodrigues@grendene.com.br>
2022-10-25 11:19:54 -0300

    tela Cidades

---
+++ src/pages/cidades/ListagemDeCidades.tsx
@@ -0,0 +1,29 @@
+import { useMemo } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { FerramentasDaListagem } from '../../shared/components';
+import { LayoutBaseDePagina } from '../../shared/layouts';
+
+export function ListagemDeCidade (){
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const busca = useMemo(() => {
+    return searchParams.get('busca') || '';
+  }, [searchParams]);
+
+  return (
+    <LayoutBaseDePagina 
+      titulo='Listagem de cidades'
+      barraDeFerramentas = {
+        <FerramentasDaListagem
+          mostrarInputBusca 
+          textoBotaoNovo='Nova'
+          textoDaBusca={searchParams.get('busca') ?? ''}
+          aoMudarTextoDeBusca={texto => setSearchParams({busca : texto}, { replace: true})}
+        />
+      }
+    >
+
+
+    </LayoutBaseDePagina>
+  ); 
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { Dashboard } from '../pages';
+import { Dashboard, ListagemDeCidade } from '../pages';
 import { useDrawerContext } from '../shared/contexts';
 
 export function AppRoutes (): JSX.Element{
@@ -14,14 +14,22 @@ export function AppRoutes (): JSX.Element{
         path: '/pagina-inicial',
         label: 'Página Inicial'
       },
+      {
+        icon: 'location_city',
+        path: '/cidades',
+        label: 'Cidades'
+      },
     ]);
   }, []);
   
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={
-        <Dashboard/>
-      }/>
+
+      <Route path="/pagina-inicial" element={<Dashboard/>}/>
+
+      <Route path="/cidades" element={<ListagemDeCidade/>}/>
+      {/* <Route path="/cidades" element={<ListagemDeCidade/>}/> */}
+
       <Route path="*" element={<Navigate to="/pagina-inicial" />}/>
     </Routes>
   );
