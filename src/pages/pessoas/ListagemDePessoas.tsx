59ac0ceb507a54d439ed550eeccf2ffffc00fc60
Leonardo <leonardorodrigues@grendene.com.br>
2022-10-27 08:34:15 -0300

    Criado hook pesonalizado useDebounce

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -3,23 +3,28 @@ import { useSearchParams } from 'react-router-dom';
 import { FerramentasDaListagem } from '../../shared/components';
 import { LayoutBaseDePagina } from '../../shared/layouts';
 import { pessoasService } from '../../shared/services/api/pessoas/PessoasServices';
+import { useDebounce } from '../../shared/hooks/UseDebounce';
 
 export function ListagemDePessoas (){
   const [searchParams, setSearchParams] = useSearchParams();
+  const { debounce } = useDebounce(3000);
 
   const busca = useMemo(() => {
     return searchParams.get('busca') || '';
   }, [searchParams]);
 
   useEffect(() => {
-    pessoasService.getAll(1, busca)
-      .then((result) => {
-        if(result instanceof Error){
-          alert(result.message);
-          return;
-        }
-        console.log(result);
-      });
+    debounce(() => {
+      pessoasService.getAll(1, busca)
+        .then((result) => {
+          if(result instanceof Error){
+            alert(result.message);
+            return;
+          }
+          console.log(result);
+        });
+
+    });
   }, [busca]);
 
   return (
