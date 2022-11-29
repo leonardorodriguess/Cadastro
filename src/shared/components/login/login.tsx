16fabbc0261d3d4ed2a351a4880a677543647ad4
Leonardo Rodrigues <leo-feitosa@live.com>
2022-11-29 19:04:04 -0300

    Construido Tela de Login

--- src/shared/components/login/login.tsx
+++ src/shared/components/login/login.tsx
@@ -1,20 +1,108 @@
-import { Box } from '@mui/material';
+import { Box, Button, Card, CardActions, CardContent, CircularProgress, TextField, Typography} from '@mui/material';
+import { useState } from 'react';
+import * as yup from 'yup';
 import { useAuthContext } from '../../contexts';
 
 interface ILoginProps {
   children: React.ReactNode;
 }
 
+const loginSchema = yup.object().shape({
+  email: yup.string().email().required(),
+  password: yup.string().required().min(5),
+});
+
 export function Login ({children} : ILoginProps){
-  const { isAuthenticated } = useAuthContext();
+  const { isAuthenticated, login } = useAuthContext();
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
+  const handleSubmit = () => {
+    setIsLoading(true);
+
+    loginSchema
+      .validate({ email, password}, { abortEarly: false})
+      .then(dadosValidados => {
+        login(dadosValidados.email, dadosValidados.password)
+          .then(() => {
+            setIsLoading(false);
+          });
+      })
+      .catch((errors: yup.ValidationError) => {
+        setIsLoading(false);
+        
+        errors.inner.forEach(error => {
+          if(error.path === 'email'){
+            setEmailError(error.message);
+          } else if (error.path === 'password') {
+            setPasswordError(error.message);
+          }
+        });
+      });
+  };
 
   if (isAuthenticated) {
     return <>{children}</>;
   }
 
   return (
-    <Box>
-      login
+    <Box width='100vw' height='100vh' display='flex' alignItems='center' justifyContent='center'>
+      <Card>
+        <CardContent>
+          <Box display='flex' flexDirection='column' gap={2} width={250}>
+            <Typography>Identifique-se</Typography>
+
+            <TextField 
+              fullWidth
+              type='email'
+              label='Email'
+              value={email}
+              disabled={isLoading}
+              error={!!emailError}
+              helperText={emailError}
+              onKeyDown={() => setEmailError('')}
+              onChange={e => setEmail(e.target.value)}
+            />
+
+            <TextField
+              fullWidth
+              label='Senha'
+              type='password'
+              value={password}
+              disabled={isLoading}
+              error={!!passwordError}
+              helperText={passwordError}
+              onKeyDown={() => setPasswordError('')}
+              onChange={e => setPassword(e.target.value)}
+            />
+          </Box>
+          
+        </CardContent>
+        <CardActions>
+          <Box width='100%' display='flex' justifyContent='center'>
+            <Button
+              variant='contained'
+              endIcon={
+                isLoading? 
+                  <CircularProgress 
+                    variant="indeterminate" 
+                    color='inherit' 
+                    size={20} 
+                  /> : undefined
+              }
+              disabled={isLoading}
+              onClick={() => handleSubmit()}
+            >
+              Entrar
+            </Button>
+          </Box>
+        </CardActions>
+      </Card>
     </Box>
   );
 }
